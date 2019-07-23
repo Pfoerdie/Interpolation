91fec4ae5d50afddaaa53c624724a8d8ec15e6ba
Simon Petrac <pfoerdie@gmail.com>
2019-07-23 04:47:04 +0200

    new approach

--- test.js
+++ test.js
@@ -1,25 +1,18 @@
 const
-    Polynom = require("./Polynom.js"),
-    BSpline = require("./BSpline.js");
+    interpl = require("./index.js");
 
-// let f = new Polynom(0, -1, -5, 1);
-// let S = new Polynom(x => x - 1, x => x / 3, x => 3 * x ** 2 - x);
-// let B = new BSpline(0.5, 1.5, 2.5, 3.5);
-// console.log(f, S, B);
+function defaultDerive(n) {
+    if (n > 0) return (x) => n * (x ** (n - 1));
+    else return (x) => 0;
+}
 
-// for (let x = 0; x <= 4; x += .1) {
-//     console.log([x, f.calc(x), S.calc(x), B.calc(x)].map(val => Math.round(val * 1e5) / 1e5).join("\t|\t"));
-// }
+let f = interpl.Polynom([0, 0, 1]);
+let f1 = f._derive(defaultDerive);
+let B = interpl.BSpline([0, 1, 2, 3, 4]);
+let B1 = B._derive();
+let S = interpl.Spline([0, 1, 2, 3], [0, 1, 1, 0], 3);
 
-const
-    min = 0,
-    max = 100,
-    step = 2.5,
-    args = (new Array(Math.trunc((max - min) / step))).fill(null).map((val, index) => min + step * index),
-    deg = 2,
-    splines = (new Array(args.length - deg - 2)).fill(null).map((val, index) => new BSpline(...args.slice(index, index + deg + 2))),
-    falseInterPoly = new Polynom(...splines.map(spline => spline.calc.bind(spline)));
-
-for (let x = min; x <= max; x += step) {
-    console.log([x, falseInterPoly.calc(x)].join("\t|\t"));
+console.log("[x, f(x), f1(x), B(x), B1(x)]");
+for (let x = 0; x <= 4; x += .5) {
+    console.log([x, f(x), f1(x), B(x), B1(x)]);
 }
