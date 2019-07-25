0ed03827ad0e406abcc4174b28c1a9ea943b6323
Simon Petrac <pfoerdie@gmail.com>
2019-07-26 01:34:15 +0200

    Polynom and BSpline working

--- Interpolation.js
+++ Interpolation.js
@@ -1,42 +1,47 @@
 const _ = require("./tools.js");
 
+function _defaultBase(n) {
+    return (x) => x ** n;
+} // _defaultBase
+
+function _defaultDeriv(n, k) {
+    if (n - k < 0) return (x) => 0;
+    let j = n - k, q = 1;
+    for (let i = j; i <= n; i++)
+        q *= i;
+    return (x) => q * (x ** j);
+} // _defaultDeriv
+
 _.define(exports, 'Polynom', Polynom);
-function Polynom(coeffs, baseFn, options = {}) {
-    _.assert(_.is.array(coeffs) && coeffs.length > 1 && coeffs.every(_.is.number));
-    if (!baseFn) {
-        baseFn = (coeff, n) => {
-            return x => coeff * (x ** n);
-        };
-        options.deriv = [];
-        for (let k = 1; k < coeffs.length; k++) {
-            let factor = 1;
-            let derivFn = (coeff, n) => {
-                if (n - k < 0) return x => 0;
-                let nk = n - k;
-                for (let i = nk; i <= n; i++)
-                    coeff *= i;
-                return x => coeff * (x ** nk);
-            };
-            options.deriv.push(derivFn);
-        }
-    };
-    let base = _.is.function(baseFn) ? coeffs.map(baseFn) : baseFn;
-    _.assert(_.is.array(base) && base.length == coeffs.length && base.every(_.is.function));
-    if (!options.deriv) options.deriv = [];
-    _.assert(_.is.array(options.deriv));
-    options.deriv = options.deriv.map(derivFn => _.is.function(derivFn) ? coeffs.map(derivFn) : derivFn);
-    _.assert(options.deriv.every(deriv => _.is.array(deriv) && deriv.length == coeffs.length && deriv.every(_.is.function)));
-    let P = (x) => coeffs.reduce((sum, coeff, n) => sum + base[n](x), 0);
+function Polynom(coeffs, base, ...derivations) {
+    _.assert(_.is.array(coeffs) && coeffs.every(_.is.number));
+    _.assert(coeffs.length > 1);
+    if (base) {
+        if (_.is.function(base))
+            base = coeffs.map((coeff, n) => base(n));
+        _.assert(_.is.array(base) && base.every(_.is.function));
+        _.assert(base.length == coeffs.length);
+        derivations = derivations.map(deriv => _.is.function(deriv) ? coeffs.map((coeff, n) => deriv(n)) : deriv);
+        _.assert(derivations.every(derivBase => _.is.array(derivBase) && derivBase.length == coeffs.length && derivBase.every(_.is.function)));
+    } else {
+        _.assert(derivations.length == 0);
+        base = coeffs.map((coeff, n) => _defaultBase(n));
+        for (let k = 1; k < coeffs.length; k++)
+            derivations.push(coeffs.map((coeff, n) => _defaultDeriv(n, k)));
+    }
+    let P = (x) => coeffs.reduce((sum, coeff, n) => sum + coeff * base[n](x), 0);
     _.define(P, 'derive', function (k = 1) {
-        _.assert(_.is.number(k) && k > 0 && k <= options.deriv.length);
-        return Polynom(coeffs, options.deriv[k - 1], { deriv: options.deriv.slice(k) });
+        _.assert(_.is.number(k) && k > 0);
+        _.assert(k <= derivations.length);
+        return Polynom(coeffs, ...(derivations.slice(k - 1)));
     });
     return P;
 } // Polynom
 
 _.define(exports, 'BSpline', BSpline);
 function BSpline(args) {
-    _.assert(_.is.array(args) && args.length > 2 && args.every(_.is.number) && args.every((val, index) => index == 0 || val > args[index - 1]));
+    _.assert(_.is.array(args) && args.length > 2 && args.every(_.is.number));
+    _.assert(args.length > 2 && args.every((val, index) => index == 0 || val > args[index - 1]));
     let coeffs = new Array(args.length);
     for (let k = 0; k < args.length; k++) {
         coeffs[k] = args.length - 1;
@@ -44,8 +49,18 @@ function BSpline(args) {
             if (i != k) coeffs[k] *= 1 / (args[k] - args[i]);
     }
     let deg = coeffs.length - 2;
-    let P = Polynom(coeffs, (coeff, n) => (x => coeff * (Math.max(0, args[n] - x) ** deg)));
-    // TODO
+    let genFns = [];
+    let xs = args[0];
+    let i = 1;
+    for (let j = deg; j >= 0; j--) {
+        let q = i;
+        genFns.push((n) => {
+            let xn = args[n];
+            return (x) => (x > xs && x < xn) ? q * ((xn - x) ** j) : 0;
+        });
+        i *= -j;
+    }
+    return Polynom(coeffs, ...genFns);
 } // BSpline
 
 _.define(exports, 'Spline', Spline);
--- test.js
+++ test.js
@@ -1,12 +1,21 @@
 const interpl = require("./Interpolation.js");
 
 let f = interpl.Polynom([0, 0, 1]);
-let f1 = f.derive(2);
-// let B = interpl.BSpline([0, 1, 2, 3, 4]);
-// let B1 = B._derive();
+let f1 = f.derive();
+let B = interpl.BSpline([0, 1, 2, 3, 4]);
+let B1 = B.derive();
+let B2 = B1.derive();
 // let S = interpl.Spline([0, 1, 2, 3], [0, 1, 1, 0], [0, 0]);
 
-console.log("[x, f(x), f1(x), B(x), B1(x), S(x)]");
-for (let x = 0; x <= 4; x += .5) {
-    console.log([x, f(x), f1(x)]);
+let data = [];
+for (let x = -.5; x <= 4.5; x += .25) {
+    data.push({
+        'x': x,
+        'f(x)': f(x),
+        'f1(x)': f1(x),
+        'B(x)': B(x),
+        'B1(x)': B1(x),
+        'B2(x)': B2(x)
+    });
 }
+console.table(data);
